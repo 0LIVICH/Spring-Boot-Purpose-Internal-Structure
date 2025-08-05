3fb0020e13b3c7cfb74069c66dde1c71971e4dcd
0LIVICH <tagir.nuretdinow@yandex.ru>
2025-08-05 11:11:26 +0300

    Delete Spring boot directory

--- Spring boot/src/main/java/com/example/profile/ProductionProfile.java
+++
@@ -1,8 +0,0 @@
-package com.example.profile;
-
-public class ProductionProfile implements SystemProfile {
-    @Override
-    public String getProfile() {
-        return "Current profile is production";
-    }
-} 
--- Spring boot/src/main/java/com/example/profile/SystemProfile.java
+++
@@ -1,5 +0,0 @@
-package com.example.profile;
-
-public interface SystemProfile {
-    String getProfile();
-} 
